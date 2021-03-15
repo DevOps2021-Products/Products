9f278f96995ccd38024c1970ba05df826fce85bd
feifan-fiona <fidodidoff@hotmail.com>
2021-03-15 16:17:54 +0800

    Added create account with tests

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,5 +1,5 @@
 """
-<your resource name> API Service Test Suite
+Product API Service Test Suite
 
 Test cases can be run with the following:
   nosetests -v --with-spec --spec-color
@@ -10,39 +10,94 @@ import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 from flask_api import status  # HTTP Status Codes
-from service.models import db
+from service.models import db, Product
 from service.routes import app, init_db
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
+)
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
-class TestYourResourceServer(TestCase):
+class TestProductServer(TestCase):
     """ REST API Server Tests """
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
-        pass
-
-    @classmethod
-    def tearDownClass(cls):
-        """ This runs once after the entire test suite """
-        pass
+        """ Run once before all tests """
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        init_db()
 
     def setUp(self):
-        """ This runs before each test """
+        """ Runs before each test """
+        db.drop_all()  # clean up the last tests
+        db.create_all()  # create new tables
         self.app = app.test_client()
 
-
     def tearDown(self):
-        """ This runs after each test """
-        pass
+        db.session.remove()
+        db.drop_all()
+
+    def _create_product(self):
+        return Product(
+            sku=12345,
+            name="ABCchocolate", 
+            category="food", 
+            short_description="dark chocolate",
+            long_description="lindts dark chocolate Christmas limited version",
+            price=28,
+            rating=4, 
+            stock_status=True
+        )
 
 ######################################################################
 #  P L A C E   T E S T   C A S E S   H E R E 
 ######################################################################
-
     def test_index(self):
-        """ Test index call """
+        """ Test the Home Page """
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        # data = resp.get_json()
+        # self.assertEqual(data["name"], "Product REST API Service")
+
+    def test_create_product(self):
+        """ Create a new Product """
+        test_product = self._create_product()
+        logging.debug(test_product)
+        resp = self.app.post(
+            "/products", json=test_product.serialize(), content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        
+        # Make sure location header is set
+        location = resp.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_product = resp.get_json()
+        self.assertEqual(new_product["sku"], test_product.sku)
+        self.assertEqual(new_product["name"], test_product.name)
+        self.assertEqual(new_product["category"], test_product.category)
+        self.assertEqual(new_product["short_description"], test_product.short_description)
+        self.assertEqual(new_product["long_description"], test_product.long_description)
+        self.assertEqual(new_product["price"], test_product.price)
+        self.assertEqual(new_product["rating"], test_product.rating)
+        self.assertEqual(new_product["stock_status"], test_product.stock_status)
+
+        # ToDo: Uncomment once retrieve product is implemented
+        # # Check that the location header was correct
+        # resp = self.app.get(location, content_type="application/json")
+        # self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        # new_product = resp.get_json()
+        # self.assertEqual(new_product["sku"], test_product.sku)
+        # self.assertEqual(new_product["name"], test_product.name)
+        # self.assertEqual(new_product["category"], test_product.category)
+        # self.assertEqual(new_product["short_description"], test_product.short_description)
+        # self.assertEqual(new_product["long_description"], test_product.long_description)
+        # self.assertEqual(new_product["price"], test_product.price)
+        # self.assertEqual(new_product["rating"], test_product.rating)
+        # self.assertEqual(new_product["stock_status"], test_product.stock_status)
