04883cc1cdb7f9a697852a43303f6ec83b44ec3e
Joseph Kiley <josephkiley@icloud.com>
2021-04-07 17:19:07 -0400

    indentation change, hopefully allows travis to run

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ import json
 
 # Get configuration from environment
 DATABASE_URI = os.getenv(
-"DATABASE_URI",
-"postgres://postgres:postgres@localhost:5432/postgres"
+    "DATABASE_URI",
+    "postgres://postgres:postgres@localhost:5432/postgres"
 )
 # override if we are running in Cloud Foundry
 if 'VCAP_SERVICES' in os.environ:
-vcap = json.loads(os.environ['VCAP_SERVICES'])
-creds = vcap['cloudantNoSQLDB'][0]['credentials']
-hostname = creds['host']
-password = creds['password']
-port = int(creds['port'])
-DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    creds = vcap['cloudantNoSQLDB'][0]['credentials']
+    hostname = creds['host']
+    password = creds['password']
+    port = int(creds['port'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
