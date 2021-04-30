6fbdbe09b090c912a3fa49416c1e340bfd19c714
Joseph Kiley <josephkiley@icloud.com>
2021-04-29 21:38:02 -0400

    change to config file

--- config.py
+++ config.py
@@ -16,11 +16,8 @@ DATABASE_URI = os.getenv(
 # override if we are running in Cloud Foundry
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.environ['VCAP_SERVICES'])
-    creds = vcap['cloudantNoSQLDB'][0]['credentials']
-    hostname = creds['host']
-    password = creds['password']
-    port = int(creds['port'])
     DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
