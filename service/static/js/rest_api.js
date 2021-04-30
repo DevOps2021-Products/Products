92132cf4443d39256f611d9b0f761042c0bbe7bf
Joseph Kiley <josephkiley@icloud.com>
2021-04-29 21:26:00 -0400

    Changes to get the tests passing

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -202,12 +202,31 @@ $(function () {
     $("#like-btn").click(function () {
 
         var product_id = $("#product_id").val();
+        var name = $("#product_name").val();
+        var sku = $("#product_sku").val();
+        var category = $("#product_category").val();
+        var price = $("#product_price").val();
+        var available = $("#product_available").val() == "true";
+        var rating = $("#product_rating").val();
+        var short_description = $("$product_short_description").val;
+        var enabled = $("#product_enabled").val() == "true";
+
+        var data = {
+            "name": name,
+            "sku": sku,
+            "category": category,
+            "price": price,
+            "available": available,
+            "rating": rating,
+            "short_description": short_description,
+            "enabled": enabled
+        };
 
         var ajax = $.ajax({
             type: "PUT",
             url: "/products/" + product_id + "/like",
             contentType: "application/json",
-            data: '',
+            data: JSON.stringify(data)
         })
 
         ajax.done(function(res){
@@ -227,12 +246,31 @@ $(function () {
     $("#disable-btn").click(function () {
 
         var product_id = $("#product_id").val();
+        var name = $("#product_name").val();
+        var sku = $("#product_sku").val();
+        var category = $("#product_category").val();
+        var price = $("#product_price").val();
+        var available = $("#product_available").val() == "true";
+        var rating = $("#product_rating").val();
+        var short_description = $("$product_short_description").val;
+        var enabled = $("#product_enabled").val() == "true";
+
+        var data = {
+            "name": name,
+            "sku": sku,
+            "category": category,
+            "price": price,
+            "available": available,
+            "rating": rating,
+            "short_description": short_description,
+            "enabled": enabled
+        };
 
         var ajax = $.ajax({
             type: "PUT",
             url: "/products/" + product_id + "/disable",
             contentType: "application/json",
-            data: '',
+            data: JSON.stringify(data)
         })
 
         ajax.done(function(res){
